12969f694c41dd08da45ed4402c292ac96be83ba
yjx <791231666@qq.com>
2017-12-19 17:19:30 +0800

    Initial commit

---
+++ src/main/java/kurento/wisonic/test/jfcfg/Config.java
@@ -0,0 +1,40 @@
+package kurento.wisonic.test.jfcfg;
+
+import com.jfinal.config.*;
+import com.jfinal.template.Engine;
+
+/**
+ * @author 木数难数
+ */
+public class Config extends JFinalConfig{
+
+    @Override
+    public void configConstant(Constants constants) {
+
+    }
+
+    @Override
+    public void configRoute(Routes routes) {
+        
+    }
+
+    @Override
+    public void configEngine(Engine engine) {
+
+    }
+
+    @Override
+    public void configPlugin(Plugins plugins) {
+
+    }
+
+    @Override
+    public void configInterceptor(Interceptors interceptors) {
+
+    }
+
+    @Override
+    public void configHandler(Handlers handlers) {
+
+    }
+}
